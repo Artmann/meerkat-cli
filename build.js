1013e1cc31938e19d44b933a88e1a1e7f36f965a
Christoffer Artmann <artgaard@gmail.com>
2025-03-12 23:54:49 +0100

    Initial commit.

---
+++ build.js
@@ -0,0 +1,17 @@
+const esbuild = require("esbuild");
+
+esbuild
+  .build({
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    platform: "node",
+    target: "node14",
+    outfile: "dist/cli.cjs",
+    format: "cjs", // Output as CommonJS
+    minify: false,
+    banner: {
+      js: "#!/usr/bin/env node",
+    },
+  })
+  .then(() => console.log("Build complete!"))
+  .catch(() => process.exit(1));
